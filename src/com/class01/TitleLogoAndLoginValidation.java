c655b562247d16ecf1dde45a0dd34378e350d55d
elviraSamakova <elvira.samakova@gmail.com>
2020-05-20 12:53:24 -0400

    first class of TestNG

---
+++ src/com/class01/TitleLogoAndLoginValidation.java
@@ -0,0 +1,62 @@
+package com.class01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.*;
+
+import com.utils.CommonMethods;
+import com.utils.ConfigsReader;
+
+public class TitleLogoAndLoginValidation extends CommonMethods {
+
+	@BeforeMethod
+	public void openAndNavigate() {
+		setUp();
+	}
+
+	@AfterMethod
+	public void quitBrowser() {
+		tearDown();
+	}
+
+	@Test
+	public void titleValidation() {
+		String expectedTitle = "Human Management System";
+		String actualTitle = driver.getTitle();
+		if (expectedTitle.equals(actualTitle)) {
+			System.out.println("Title Passed");
+		} else {
+			System.out.println("Title Failed");
+		}
+
+	}
+
+	@Test
+	public void logoValidation() {
+		String logoPath = "//div[@id='divLogo']/img";
+		WebElement logo = driver.findElement(By.xpath("//div[@id='divLogo']/img"));
+		boolean isDisplayed = logo.isDisplayed();
+		if (isDisplayed) {
+			System.out.println("Logo passed");
+		} else {
+			System.out.println("Logo Failed");
+
+		}
+
+	}
+
+	@Test
+	public void validValidation() {
+
+		WebElement username=driver.findElement(By.id("txtUsername"));
+		sendText(username, ConfigsReader.getProperty("username"));
+		
+		WebElement password=driver.findElement(By.id("txtPassword"));
+		sendText(password, ConfigsReader.getProperty("password"));
+		
+		click(driver.findElement(By.id("btnLogin")));
+		
+		
+	}
+
+}
