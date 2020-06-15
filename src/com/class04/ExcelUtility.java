8d0b8a22f776948e69350e025c6070065885eab1
elviraSamakova <elvira.samakova@gmail.com>
2020-06-15 13:25:51 -0400

    update

--- src/com/class04/ExcelUtility.java
+++ src/com/class04/ExcelUtility.java
@@ -1,14 +1,18 @@
 package com.class04;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class ExcelUtility {
 
-	public static WorkBoook book;
+	public static Workbook book;
 	public static Sheet sheet;
 
-	public static void openExcel(String filepath) {// filename=filepath same thing
+	public static void openExcel(String filepath) throws FileNotFoundException {// filename=filepath same thing
 
 		FileInputStream fis = new FileInputStream(filepath);
 		try {
